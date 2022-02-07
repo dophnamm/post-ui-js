d069c1bbfc95c9ac8aaabf70c122a82cc77ae0ab
Phuong Nam <dophnamm@gmail.com>
2022-02-07 13:38:51 +0700

    render post to UI and add dayjs library format timestamp

--- js/api/axiosClient.js
+++ js/api/axiosClient.js
@@ -7,4 +7,30 @@ const axiosClient = axios.create({
 	},
 });
 
+axiosClient.interceptors.request.use(
+	function (config) {
+		const accessToken = localStorage.getItem('access_token');
+
+		if (accessToken) {
+			config.headers.Authorization = `Baerer ${accessToken}`;
+		}
+
+		return config;
+	},
+
+	function (error) {
+		return Promise.reject(error);
+	}
+);
+
+axiosClient.interceptors.response.use(
+	function (response) {
+		return response.data;
+	},
+
+	function (error) {
+		return Promise.reject(error);
+	}
+);
+
 export default axiosClient;
--- js/main.js
+++ js/main.js
@@ -1,8 +1,55 @@
 import postApi from './api/postApi';
+import { setTextContent, truncateText } from './utils/';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from 'dayjs';
 
-const main = async () => {
-	const response = await postApi.getAll();
-	console.log(response);
-};
+dayjs.extend(relativeTime);
 
-main();
+function createTemplateElement(post) {
+	try {
+		const templateElement = document.getElementById('postItemTemplate');
+		if (!templateElement) return;
+
+		const liElement = templateElement.content.firstElementChild.cloneNode(true);
+		if (!liElement) return;
+
+		const thumbElement = liElement.querySelector('[data-id="thumbnail"]');
+		thumbElement.src = post.imageUrl;
+		thumbElement.addEventListener('error', () => {
+			thumbElement.src = 'https://via.placeholder.com/1378x400?text=dophnamm@gmail.com';
+		});
+
+		setTextContent(liElement, '[data-id="title"]', post.title);
+		setTextContent(liElement, '[data-id="description"]', truncateText(post.description, 100));
+		setTextContent(liElement, '[data-id="author"]', post.author);
+		setTextContent(liElement, '[data-id="timeSpan"]', dayjs(post.updatedAt).fromNow());
+
+		return liElement;
+	} catch (error) {
+		console.log('failure from createTemplate', error);
+	}
+}
+
+function renderPostList(postList) {
+	if (!Array.isArray(postList) || postList.length === 0) return;
+	const ulElement = document.getElementById('postsList');
+
+	postList.map((post) => {
+		const liElement = createTemplateElement(post);
+		ulElement.appendChild(liElement);
+	});
+}
+
+(async () => {
+	try {
+		const config = {
+			_page: 1,
+			_limit: 6,
+		};
+
+		const data = await postApi.getAll(config);
+		renderPostList(data.data);
+	} catch (error) {
+		console.log('failure from main.js call api getAll', error);
+	}
+})();
